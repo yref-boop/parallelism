9dc8f26f8191257a57bc42fc02d7c131078b5dca
yref-boop <regofdez.yago@gmail.com>
2022-04-30 12:50:18 +0200

    feat: added option to print the computation time of each thread

--- p3/matvec.c
+++ p3/matvec.c
@@ -3,15 +3,16 @@
 #include <mpi.h>
 #include <sys/time.h>
 
-#define DEBUG 1
+#define DEBUG 0
 
-#define N 4
+#define N 49
 
 // expected result -> 14 20 26 32
 
 int main(int argc, char *argv[] ) {
 
-	int i, j, rows;
+	int i, j, rows, local_msecs; 
+	int *total_msecs;
 	float *local_matrix, *local_result;
 	float matrix[N][N];
 	float vector[N];
@@ -46,28 +47,34 @@ int main(int argc, char *argv[] ) {
 	// scatter matrix data
 	MPI_Scatter (matrix, rows*N, MPI_FLOAT, local_matrix, rows*N, MPI_FLOAT, 0, MPI_COMM_WORLD);
 
+	gettimeofday(&tv1, NULL);
 	// calculate result
   	for(i=0;i<rows;i++) {
 		local_result[i]=0;
 		for(j=0;j<N;j++)
 			local_result[i] += local_matrix[N*i+j]*vector[j];
 	}
+	gettimeofday(&tv2, NULL);
 
 	// get value of time
-	int microseconds = (tv2.tv_usec - tv1.tv_usec) + 1000000 * (tv2.tv_sec - tv1.tv_sec);
+	local_msecs = (tv2.tv_usec - tv1.tv_usec) + 1000000 * (tv2.tv_sec - tv1.tv_sec);
 
-	if (rank == 0)
+	if (rank == 0){
 		result = (float*) malloc(sizeof(float)*N*numprocs*rows);
+		total_msecs = (int *) malloc(sizeof(int)*numprocs);
+	}
 
 	// gather data
 	MPI_Gather (local_result, rows, MPI_FLOAT, result, rows, MPI_FLOAT, 0, MPI_COMM_WORLD);
+	MPI_Gather (&local_msecs, 1, MPI_INT, total_msecs, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
   	// display result
 	if (rank == 0) {
 		if (DEBUG)
 			for(i=0;i<N;i++) printf(" %f \t ", result[i]);
-		else 
-			printf ("Time (seconds) = %lf\n", (double) microseconds/1E6);
+		else
+			for (i=0;i<numprocs;i++)
+			printf ("time (seconds) of process %d= %lf\n", rank, (double) total_msecs[i]/1E6);
 	}
 
 	// finalize all threads
