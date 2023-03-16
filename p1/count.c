663f5a58c46cb7d379d710206ab6e4fefae1c908
yref-boop <regofdez.yago@gmail.com>
2023-03-16 12:52:26 +0100

    refactor: changed variable & functions names + code style

--- p1/count.c
+++ p1/count.c
@@ -2,46 +2,45 @@
 #include <stdlib.h>
 #include <math.h>
 
-void inicializaCadena(char *cadena, int n){
-  int i;
-  for(i=0; i<n/2; i++){
-    cadena[i] = 'A';
-  }
-  for(i=n/2; i<3*n/4; i++){
-    cadena[i] = 'C';
-  }
-  for(i=3*n/4; i<9*n/10; i++){
-    cadena[i] = 'G';
-  }
-  for(i=9*n/10; i<n; i++){
-    cadena[i] = 'T';
-  }
+void initialize_string (char *string, int n){
+    int i;
+    for (i=0; i<n/2; i++) {
+        string[i] = 'A';
+    }
+    for (i=n/2; i<3*n/4; i++) {
+        string[i] = 'C';
+    }
+    for (i=3*n/4; i<9*n/10; i++) {
+        string[i] = 'G';
+    }
+    for (i=9*n/10; i<n; i++) {
+        string[i] = 'T';
+    }
 }
 
-int main(int argc, char *argv[])
-{
-  if(argc != 3){
-    printf("Numero incorrecto de parametros\nLa sintaxis debe ser: program n L\n  program es el nombre del ejecutable\n  n es el tamaño de la cadena a generar\n  L es la letra de la que se quiere contar apariciones (A, C, G o T)\n");
-    exit(1); 
-  }
-    
-  int i, n, count=0;
-  char *cadena;
-  char L;
-
-  n = atoi(argv[1]);
-  L = *argv[2];
-  
-  cadena = (char *) malloc(n*sizeof(char));
-  inicializaCadena(cadena, n);
-  
-  for(i=0; i<n; i++){
-    if(cadena[i] == L){
-      count++;
+int main (int argc, char *argv[]) {
+    if (argc != 3){
+        printf("Incorrect argument number\nsyntaxis should follow: program n L\n  program (program name)\n  n (string size)\n  L (character to look for) (A, C, G o T)\n");
+        exit(1);
     }
-  }
-  
-  printf("El numero de apariciones de la letra %c es %d\n", L, count);
-  free(cadena);
-  exit(0);
+ 
+    int i, n, count=0;
+    char *string;
+    char L;
+
+    n = atoi (argv[1]);
+    L = *argv[2];
+ 
+    string = (char *) malloc (n*sizeof (char));
+    initialize_string (string, n);
+ 
+    for (i=0; i<n; i++){
+      if (string[i] == L){
+        count++;
+      }
+    }
+ 
+    printf ("character %c appears %d times\n", L, count);
+    free (string);
+    exit (0);
 }
