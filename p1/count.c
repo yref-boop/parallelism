ecf5389f9c27922a4ca037f51776f17dc0d78368
yref-boop <regofdez.yago@gmail.com>
2023-04-18 20:55:56 +0200

    wip: process #0 is in charge of IO

--- p1/count.c
+++ p1/count.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <mpi.h>
 
 void initialize_string (char *string, int n){
     int i;
-    for (i=0; i<n/2; i++) {
+
+    for (i=0; i<n/2; i++)
         string[i] = 'A';
-    }
-    for (i=n/2; i<3*n/4; i++) {
+
+    for (i=n/2; i<3*n/4; i++)
         string[i] = 'C';
-    }
-    for (i=3*n/4; i<9*n/10; i++) {
+
+    for (i=3*n/4; i<9*n/10; i++)
         string[i] = 'G';
-    }
-    for (i=9*n/10; i<n; i++) {
+
+    for (i=9*n/10; i<n; i++)
         string[i] = 'T';
-    }
 }
 
 int main (int argc, char *argv[]) {
@@ -24,23 +25,60 @@ int main (int argc, char *argv[]) {
         exit(1);
     }
  
-    int i, n, count=0;
+    int i, n, count = 0;
     char *string;
     char L;
 
-    n = atoi (argv[1]);
-    L = *argv[2];
- 
-    string = (char *) malloc (n*sizeof (char));
-    initialize_string (string, n);
- 
+    // auxiliar variables
+    int rec_n;
+    char rec_L;
+    int recv_count;
+    int numprocs, rank;
+
+    // needed mpi variables
+    MPI_Status status;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size (MPI_COMM_WORLD, &numprocs);
+
+    if (rank == 0) {
+
+        // get input values on first thread
+        n = atoi (argv[1]);
+        L = *argv[2];
+
+        // share input values with other threads
+        int i = numprocs;
+        while (i-- > 1) {
+            MPI_Send (&n, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+            MPI_Send (&L, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+        }
+
+    }
+    else {
+        // get input values from thread #0
+        MPI_Recv (&n, 1, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+        MPI_Recv (&n, 1, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+    }
+
     for (i=0; i<n; i++){
       if (string[i] == L){
         count++;
       }
     }
  
-    printf ("character %c appears %d times\n", L, count);
-    free (string);
-    exit (0);
+    if (rank > 0)
+        // any process other than 0 sends its number of occurences
+        MPI_Send (&count, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+    else {
+        // get count from all processes
+        int i = numprocs;
+        while (i-- > 1) {
+            MPI_Recv (&recv_count, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+            count += recv_count;
+        }
+        // print the results
+        printf ("character %c appears %d times\n", L, count);
+    }
+    // finalize all processes
+    MPI_Finalize();
 }
