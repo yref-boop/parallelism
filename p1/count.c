f5fa089357a945211b2b2be933133ed7e58c9955
yref-boop <regofdez.yago@gmail.com>
2023-04-18 21:36:38 +0200

    feat: each thread now computes its own count & sends it to 0

--- p1/count.c
+++ p1/count.c
@@ -52,30 +52,43 @@ int main (int argc, char *argv[]) {
             MPI_Send (&n, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
             MPI_Send (&L, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
         }
-
     }
-    else {
+
+    if (rank) {
+
         // get input values from thread #0
         MPI_Recv (&n, 1, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-        MPI_Recv (&n, 1, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+        MPI_Recv (&L, 1, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+
+        printf ("%d, %d\n", rank, n);
+        // initialize string
+        string = (char *) malloc (n*sizeof (char));
+        initialize_string (string, n);
+
+        // count ocurences in this thread
+        count = 0;
+
+        int pos = rank;
+        while (pos += numprocs < n)
+            if (string[pos] == L)
+                count++;
+
+        printf ("process %d\ncount; %d\nstring: %s", rank, count, string);
+        free (string);
+
+        // send the number of occurences to process 0
+        MPI_Send (&count, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
     }
 
-    for (i=0; i<n; i++){
-      if (string[i] == L){
-        count++;
-      }
-    }
- 
-    if (rank > 0)
-        // any process other than 0 sends its number of occurences
-        MPI_Send (&count, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
-    else {
+    if (!rank) {
+
         // get count from all processes
-        int i = numprocs;
-        while (i-- > 1) {
+        int j = numprocs;
+        while (j-- < 1) {
             MPI_Recv (&recv_count, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
             count += recv_count;
         }
+
         // print the results
         printf ("character %c appears %d times\n", L, count);
     }
