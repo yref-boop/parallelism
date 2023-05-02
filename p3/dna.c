cc8cc6d5452a182417bdee66337ccd94ff03e343
yref-boop <regofdez.yago@gmail.com>
2023-05-02 03:27:57 +0200

    feat: paralellized code

--- p3/dna.c
+++ p3/dna.c
@@ -43,47 +43,126 @@ int base_distance (int base1, int base2){
 
 int main (int argc, char *argv[] ) {
 
-    int i, j;
+    int i, j, n;
     int *data1, *data2;
     int *result;
-    struct timeval  tv1, tv2;
+    struct timeval tv1, tv2, tv3, tv4;
 
-    data1  = (int *) malloc (M * N * sizeof (int));
-    data2  = (int *) malloc (M * N * sizeof (int));
-    result = (int *) malloc (M * sizeof (int));
+    int block_size;
+    int *partial_data1, *partial_data2;
+    int *partial_result;
+    int rank, processes;
 
-    // initialize matrices randomly with 20% gap proportion
-    for (i = 0; i < M; i++)
-        for (j = 0; j < N; j++) {
-            data1 [i*N+j] = fast_rand();
-            data2 [i*N+j] = fast_rand();
-        }
+    int local_block_size;
+
+    // special tags for times
+    int communication_tag = 111;
+    int processing_tag = 59;
+
+    MPI_Init(&argc, &argv);
+
+    MPI_Status status;
+    MPI_Comm_size (MPI_COMM_WORLD, &processes);
+    MPI_Comm_rank (MPI_COMM_WORLD, &rank);
+
+    // calculate block_size
+    block_size = M / processes + (M % processes ? 1 : 0);
+
+    // initialize matrices on first thread
+    if (!rank) {
+
+        printf ("total processes: %d\nmatrix size %d x %d\n", processes, M, N);
+
+        data1  = (int *) malloc (M * N * sizeof (int));
+        data2  = (int *) malloc (M * N * sizeof (int));
+        result = (int *) malloc (M * sizeof (int));
+
+        // initialize matrices randomly with 20% gap proportion
+        for (i = 0; i < M; i++)
+            for (j = 0; j < N; j++) {
+                data1 [i*N+j] = fast_rand();
+                data2 [i*N+j] = fast_rand();
+            }
+    }
+
+    // allocate partial vectors
+    partial_data1 = (int *) malloc (block_size * N * sizeof (int));
+    partial_data2 = (int *) malloc (block_size * N * sizeof (int));
+    partial_result = (int *) malloc (block_size * sizeof (int));
 
     gettimeofday (&tv1, NULL);
 
-    for (i = 0; i < M; i++) {
-        result[i]=0;
-        for (j = 0; j < N; j++)
-            result[i] += base_distance (data1[i*N+j], data2[i*N+j]);
-    }
+    // scatter data from process #0
+    MPI_Scatter (data1, block_size * N, MPI_INT, partial_data1, block_size * N, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Scatter (data2, block_size * N, MPI_INT, partial_data2, block_size * N, MPI_INT, 0, MPI_COMM_WORLD);
+
     gettimeofday (&tv2, NULL);
 
-    int microseconds = (tv2.tv_usec - tv1.tv_usec)+ 1000000 * (tv2.tv_sec - tv1.tv_sec);
+    // redefinition of block_size on last process
+    if (rank == processes - 1)
+        local_block_size = M - block_size * (processes-1);
+    else
+        local_block_size = block_size;
 
-    // display result
-    if (DEBUG == 1) {
+    // calculate partial result on all processes
+    for (i = 0; i < local_block_size; i++) {
+        partial_result[i]=0;
+        for (j = 0; j < N; j++)
+            partial_result[i] += base_distance (partial_data1[i*N+j], partial_data2[i*N+j]);
+    }
+
+    gettimeofday (&tv3, NULL);
+
+    // gather results
+    MPI_Gather (partial_result, block_size, MPI_INT, result, block_size, MPI_INT, 0, MPI_COMM_WORLD);
+
+    gettimeofday (&tv4, NULL);
+
+    int communication_time = (tv2.tv_usec - tv1.tv_usec) + 1000000 * (tv2.tv_sec - tv1.tv_sec) + ((tv4.tv_usec -tv3.tv_usec) + 1000000 * (tv4.tv_sec - tv3.tv_sec));
+    int processing_time = (tv3.tv_usec - tv2.tv_usec) + 1000000 * (tv3.tv_sec - tv2.tv_sec);
+
+    // display results
+    if (DEBUG == 1 && (!rank)) {
         int checksum = 0;
         for (i = 0; i < M; i++)
             checksum += result[i];
         printf("Checksum: %d\n ", checksum);
     }
-    else if (DEBUG == 2)
+    else if (DEBUG == 2 && (!rank))
         for (i = 0; i < M; i++)
             printf (" %d \t ",result[i]);
-    else
-    printf ("time (seconds) = %lf\n", (double) microseconds/1E6);
 
-    free (data1); free (data2); free (result);
+    if (!rank) {
+
+        // print communication & processing times of root process
+        printf ("root process communication time (seconds) = %lf\n", (double) communication_time/1E6);
+        printf ("root process processing time (seconds) = %lf\n", (double) processing_time/1E6);
+
+        // receive & print communication & processing times from all processes
+        for (i = 1; i < processes; i++) {
+            MPI_Recv (&communication_time, 1, MPI_INT, i, communication_tag, MPI_COMM_WORLD, &status);
+            printf ("process #%d communication time (seconds) = %lf\n", i, (double) communication_time/1E6);
+            MPI_Recv (&processing_time, 1, MPI_INT, i, processing_tag, MPI_COMM_WORLD, &status);
+            printf ("process #%d processing time (seconds) = %lf\n", i, (double) processing_time/1E6);
+
+        }
+    } else {
+        MPI_Send (&communication_time, 1, MPI_INT, 0, communication_tag, MPI_COMM_WORLD);
+        MPI_Send (&processing_time, 1, MPI_INT, 0, processing_tag, MPI_COMM_WORLD);
+    }
+
+
+    if (!rank) {
+        free (data1);
+        free (data2);
+        free (result);
+    }
+
+    free (partial_data1);
+    free (partial_data2);
+    free (partial_result);
+
+    MPI_Finalize();
 
     return 0;
 }
